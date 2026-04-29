4c9791c170392a106b0a9e28f2752d3ba963e356
rajeshmadd84 <130025114+rajeshmadd84@users.noreply.github.com>
2026-04-29 08:29:21 +0530

    update google tag

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import "../public/assets/css/main.css";
 
 import { usePathname } from "next/navigation";
+import Script from "next/script";
 import { ToastContainer } from "react-toastify";
 
 export default function RootLayout({ children }) {
@@ -21,6 +22,18 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" href="/assets/images/icon.png" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FGXD9YJ7EX"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FGXD9YJ7EX');
+          `}
+        </Script>
       </head>
       <body>
         <ToastContainer
